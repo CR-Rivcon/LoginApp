fc8d2e4554f1fc46bcd71ab41c99e0f235b9fb6d
CR-Rivcon <cr.rivcon@gmail.com>
2025-11-23 11:51:03 -0300

    Avance hasta clase 12-11 terminado, to do simple con casilla para incorporar nuevas tareas

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,19 +1,37 @@
 import { TaskItem } from '@/components/task-item';
+import { IconSymbol } from '@/components/ui/icon-symbol';
 import Title from '@/components/ui/title';
 import { Task } from '@/constants/types';
 import { useState } from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, TextInput, TouchableOpacity, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import generateRandomId from '../utils/generate-random-id';
 
 const initialTodos: Task[] = [
-  { id: '1', title: 'Comprar víveres', completed: false },
-  { id: '2', title: 'Llevar el auto al taller', completed: false },
-  { id: '3', title: 'Pagar las facturas', completed: false },
+  { id: generateRandomId(), title: 'Comprar víveres', completed: false },
+  { id: generateRandomId(), title: 'Llevar el auto al taller', completed: false },
+  { id: generateRandomId(), title: 'Pagar las facturas', completed: false },
 ];
 
 export default function Home() {
 
   const [todos, setTodos] = useState<Task[]>(initialTodos);
+  const [newTaskTitle, setNewTaskTitle] = useState<string>('');
+
+
+
+
+  const addTodo = (title : string) => {
+    if (title.trim().length === 0) return;
+
+    const newTodo: Task = {
+      id: generateRandomId(),
+      title,
+      completed: false,
+    };
+    setTodos((prevTodos) => [...prevTodos, newTodo]);
+    setNewTaskTitle('');
+  }
 
 
   const toggleTodo = (id: string) => {
@@ -30,6 +48,10 @@ export default function Home() {
   }
 
   return (
+
+
+//Componentes visuales
+
     <SafeAreaView style={styles.container}>
       <Title>Todo List</Title>
       {todos.map((task) => (
@@ -40,9 +62,32 @@ export default function Home() {
         onRemove={removeTodo}
         />
       ))}
+  <View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 16 }}>
+  <TextInput
+    style={{ flex: 1, borderWidth: 1, borderColor: '#ccc', borderRadius: 4, padding: 8 }}
+    placeholder="Nueva tarea"
+    value={newTaskTitle}
+    onChangeText={setNewTaskTitle}
+    onSubmitEditing={() => addTodo(newTaskTitle)}
+  />
+  <TouchableOpacity 
+    style={{ marginLeft: 8, height: 40, justifyContent: 'center' }} 
+    onPress={() => addTodo(newTaskTitle)}
+  >
+    <IconSymbol name="plus.circle.fill" size={40} color="#00ac70" />
+  </TouchableOpacity>
+</View>
     </SafeAreaView>
+
+//Componentes visuales
   );
 }
+
+
+
+
+
+
 
 const styles = StyleSheet.create({
   container: { flex: 1, padding: 16 },
--- components/ui/icon-symbol.tsx
+++ components/ui/icon-symbol.tsx
@@ -20,6 +20,7 @@ const MAPPING = {
   'chevron.right': 'chevron-right',
   'person.fill': 'person',
   'trash.circle': 'delete',
+  'plus.circle.fill': 'add-circle',
 } as IconMapping;
 
 /**
